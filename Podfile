815625af8c01bf41fcf417cafadf1328ad21b170
corbin <hongbin.chan@qq.com>
2017-03-24 19:46:44 +0800

    更新：初始化工程

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :osx, '10.11'
+
+target 'WeChatPlugin' do
+	pod 'JRSwizzle', '~> 1.0'
+end
---
+++ WeChatPlugin/WeChatPluginHeader.h
@@ -0,0 +1,10 @@
+//
+//  WeChatPluginHeader.h
+//  WeChatPlugin
+//
+//  Created by CorbinChen on 2017/3/24.
+//  Copyright (c) 2017年 CorbinChen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
